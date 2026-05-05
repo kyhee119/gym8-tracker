d1cec9768a27f856a71ba0bbf59cda45f76ac6bb
kyhee119 <wdaaqn6472@gmail.com>
2026-05-05 11:01:42 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='gymv2';
-const ASSETS=['/gym-tracker/','/gym-tracker/index.html','/gym-tracker/manifest.json','/gym-tracker/icon.svg','/gym-tracker/sw.js'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).catch(()=>caches.match('/index.html'))));});
